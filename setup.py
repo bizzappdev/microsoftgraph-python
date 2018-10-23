6559c6d6296bf1a09230170d9d0f01899deecb91
Ruchir Shukla <ruchir@bizzappdev.com>
2018-10-23 18:51:49 +0530

    [IMP]Backport to python2.7

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
+from __future__ import absolute_import
 import os
 from setuptools import setup
+from io import open
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='microsoftgraph-python',
-      version='0.1.3',
-      description='API wrapper for Microsoft Graph written in Python',
-      long_description=read('README.md'),
-      url='https://github.com/GearPlug/microsoftgraph-python',
-      author='Miguel Ferrer, Nerio Rincon, Yordy Gelvez',
-      author_email='ingferrermiguel@gmail.com',
-      license='GPL',
+setup(name=u'microsoftgraph-python',
+      version=u'0.1.3',
+      description=u'API wrapper for Microsoft Graph written in Python',
+      long_description=read(u'README.md'),
+      url=u'https://github.com/GearPlug/microsoftgraph-python',
+      author=u'Miguel Ferrer, Nerio Rincon, Yordy Gelvez',
+      author_email=u'ingferrermiguel@gmail.com',
+      license=u'GPL',
       packages=['microsoftgraph'],
       install_requires=[
-          'requests',
+          u'requests',
       ],
       zip_safe=False)
